3561befd09d82da2dbcdcf8397afd889e54f50ae
Asim <mohdasim8018@gmail.com>
2015-12-04 00:22:44 -0600

    help

--- FinalProject/js/src/Treemap.js
+++ FinalProject/js/src/Treemap.js
@@ -904,7 +904,7 @@ function assignColorValues(){
 
 function buildTreeMap(tree){
 
-	var margin = {top:0, right: 0, bottom: 10, left: 20},
+	var margin = {top:0, right: 0, bottom: 0, left: 0},
     width = 540 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
