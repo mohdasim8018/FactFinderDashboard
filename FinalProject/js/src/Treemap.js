eade6faeac014e65942db337861298854eb9bb3e
Kunal Thorvat <kthorv2@uic.edu>
2015-12-04 06:32:07 -0600

    Ui fixes

--- FinalProject/js/src/Treemap.js
+++ FinalProject/js/src/Treemap.js
@@ -906,8 +906,8 @@ function assignColorValues(){
 function buildTreeMap(tree){
 
 	var margin = {top:0, right: 0, bottom: 0, left: 0},
-    width = 540 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 520 - margin.left - margin.right,
+    height = 470 - margin.top - margin.bottom;
 
 
 	var colorScale = d3.scale.linear()
